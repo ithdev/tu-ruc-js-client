22ac27a1bd1511899a5340073f0aa209197b5eb6
Matias Ramirez <ramirezmatias946@gmail.com>
2024-01-30 18:03:31 -0300

    Add getContribuyenteByRucOrCI function

--- src/contribuyentes.js
+++ src/contribuyentes.js
@@ -65,6 +65,58 @@ async function getContribuyenteBySearch(search, offset = 0) {
   }
 }
 
+async function getContribuyenteByRucOrCI(ruc) {
+  if (!ruc || ruc.trim() === "" || ruc.length < 3 || ruc.length > 11) {
+    throw new CustomError(
+      "El parámetro ruc es inválido. Debe tener entre 3 y 11 caracteres."
+    );
+  }
+
+  try {
+    const response = await fetch(`${URL}/${ruc}`);
+
+    const contribuyenteJsonResponse = await response.json();
+
+    if (response.status !== 200) {
+      if (response.status === 404) {
+        throw new CustomError(
+          "No se encontraron resultados.",
+          contribuyenteJsonResponse?.message || ""
+        );
+      }
+
+      if (response.status > 400 && response.status < 500) {
+        throw new CustomError(
+          "El parámetro ruc es inválido. Debe tener entre 3 y 11 caracteres.",
+          contribuyenteJsonResponse?.message || ""
+        );
+      }
+
+      throw new CustomError(
+        "Ocurrió un error al consultar la API. Por favor, intente nuevamente o Contacta con los desarrolladores.",
+        contribuyenteJsonResponse?.message || ""
+      );
+    }
+
+    const contribuyente = contribuyenteJsonResponse.data;
+
+    if (!contribuyente) {
+      throw new CustomError(
+        "No se encontraron resultados.",
+        contribuyenteJsonResponse?.message || ""
+      );
+    }
+
+    return contribuyente;
+  } catch (error) {
+    throw new CustomError(
+      "Ocurrió un error al consultar la API. Por favor, intente nuevamente o Contacta con los desarrolladores.",
+      error?.message || error?.toString() || ""
+    );
+  }
+}
+
 module.exports = {
   getContribuyenteBySearch,
+  getContribuyenteByRucOrCI,
 };
