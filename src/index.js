3be761328146a87289095066360a168674e644b3
Matias Ramirez <ramirezmatias946@gmail.com>
2024-01-30 17:44:29 -0300

    Delete getContribuyente function and import getContribuyenteBySearch from contribuyentes

--- src/index.js
+++ src/index.js
@@ -1,18 +1,6 @@
-const URL = 'https://turuc.com.py/api/contribuyente/search';
+const { getContribuyenteBySearch } = require("./contribuyentes");
 
-export { getContribuyente };
-
-function getContribuyente(search) {
-  return fetch(`${URL}?search=${search}&page=0`)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(`Error en la solicitud: ${response.status}`);
-      }
-      return response.json();
-    })
-    .then(data => data.data)
-    .catch(error => {
-      throw error;
-    });
-}
+module.exports = {
+  getContribuyenteBySearch,
+};
 
