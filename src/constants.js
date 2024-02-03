557233ad02bc2bed788993e6bf1a73021b3310f2
Matias Ramirez <ramirezmatias946@gmail.com>
2024-02-03 11:08:27 -0300

    fix: se coloca url correcto en produccion

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-const URL = "http://localhost:8080/api/contribuyente";
+const URL = "https://turuc.com.py/api/contribuyente";
 const MAX_LENGTH = 50;
 const MIN_LENGTH = 3;
 module.exports = { URL, MAX_LENGTH, MIN_LENGTH};
