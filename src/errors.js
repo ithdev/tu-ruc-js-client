e5a17f7cbfa516b465b512df58cb0022094db398
Matias Ramirez <ramirezmatias946@gmail.com>
2024-02-03 12:14:23 -0300

    refactor: segmentamos los errores en validacion y servidor para proveer mejor manejo de error

--- src/errors.js
+++ src/errors.js
@@ -1,11 +1,61 @@
-class CustomError extends Error {
-  constructor(clientMessage, serverMessage = "") {
-    super(clientMessage);
-    this.serverMessage =
-      serverMessage.length > 0 ? serverMessage : clientMessage;
+class ValidationError extends Error {
+  constructor(message,rule) {
+    super(message);
+    this.name = this.constructor.name;
+    this.rule = rule;
+    Error.captureStackTrace(this, this.constructor);
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      stack: this.stack,
+    };
+  }
+
+  toString() {
+    return JSON.stringify(this.toJSON());
+  }
+}
+
+class ServerError extends Error {
+  constructor(
+    message,
+    serverMessage,
+    statusCode = 500,
+    errorCode,
+    debugMessage
+  ) {
+    super(message);
+    this.name = this.constructor.name;
+    this.serverMessage = serverMessage;
+    this.statusCode = statusCode;
+    this.errorCode = errorCode;
+    this.timestamp = new Date().toISOString();
+    this.debugMessage = debugMessage;
+    Error.captureStackTrace(this, this.constructor);
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      serverMessage: this.serverMessage,
+      statusCode: this.statusCode,
+      errorCode: this.errorCode,
+      timestamp: this.timestamp,
+      debugMessage: this.debugMessage,
+      stack: this.stack,
+    };
+  }
+
+  toString() {
+    return JSON.stringify(this.toJSON());
   }
 }
 
 module.exports = {
-  CustomError,
+  ValidationError,
+  ServerError,
 };
